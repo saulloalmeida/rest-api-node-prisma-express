7e0363e554694df60efede2f38957041b3c8bdf3
Saullo <saullorhamon@gmail.com>
2022-04-27 20:55:06 -0300

    add cors

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
+import cors from 'cors'
 import express, { Express } from 'express'
 import { router } from './routes'
-
 const app: Express = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(router)
 
